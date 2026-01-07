c9ec5798e650acf8e651b4b5ff86a73b2cabb021
thaibault <info@torben.website>
2026-01-07 12:31:12 +0100

    Automatic update to new version.

--- endToEndTestHelper.ts
+++ endToEndTestHelper.ts
@@ -51,3 +51,23 @@ export const waitForFinishedLoading = async (
     if (loadingSpinner && await loadingSpinner.isVisible())
         await expect(loadingSpinner).not.toBeAttached()
 }
+
+export const formatEndUserDate = (date: Date) =>
+    /*
+        NOTE: Playwright does not work with the local date time string
+        representation like it is visible in browser:
+
+        date.toLocaleDateString()
+     */
+    `${String(date.getFullYear())}-` +
+    `${String(date.getMonth() + 1).padStart(2, '0')}-` +
+    String(date.getDate()).padStart(2, '0')
+export const formatEndUserTime = (date: Date) =>
+    /*
+        NOTE: Playwright does not work with the local date time string
+        representation like it is visible in browser:
+
+        date.toLocaleTimeString()
+    */
+    `${String(date.getHours()).padStart(2, '0')}:` +
+    String(date.getMinutes()).padStart(2, '0')
